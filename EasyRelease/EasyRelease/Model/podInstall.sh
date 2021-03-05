6541bd2432cc85b9e58989f15e8957e0bd463c9a
倪刚 <niyongsheng@outlook.com>
2021-03-05 14:48:12 +0800

    [X]. Daily Update

--- EasyRelease/EasyRelease/Model/podInstall.sh
+++ EasyRelease/EasyRelease/Model/podInstall.sh
@@ -2,7 +2,7 @@
 # author:niyongsheng
 # url:https://github.com/niyongsheng/EasyRelease
 # usage:sh podInstall.sh /Users/niyongsheng/EasyRelease
-# if you installed `imagemagick` bug it not work, please run `which pod` replace `/usr/local/bin/pod`
+# if you installed `CocoaPod` bug it not work, please run `which pod` replace `/usr/local/bin/pod`
 export LANG=en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
